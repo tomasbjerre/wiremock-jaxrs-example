9ea4807fb2c581d12d4674f8fb6a6d2df2d66f40
Tomas Bjerre <tomas.bjerre85@gmail.com>
2019-04-26 19:55:56 +0200

    work

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Wiremock JAX-RS Example
+[![Build Status](https://travis-ci.org/tomasbjerre/wiremock-jaxrs-example.svg?branch=master)](https://travis-ci.org/tomasbjerre/wiremock-jaxrs-example)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/se.bjurr.wiremock/wiremock-jaxrs-example/badge.svg)](https://maven-badges.herokuapp.com/maven-central/se.bjurr.wiremock/wiremock-jaxrs-example)
+[![Bintray](https://api.bintray.com/packages/tomasbjerre/tomasbjerre/se.bjurr.wiremock%3Awiremock-jaxrs-example/images/download.svg) ](https://bintray.com/tomasbjerre/tomasbjerre/se.bjurr.wiremock%3Awiremock-jaxrs-example/_latestVersion)
+
 Example of:
 
  https://github.com/tomasbjerre/wiremock-jaxrs
--- api-mock/src/test/java/se/bjurr/wiremock/example/api/mock/CreateMocksTest.java
+++ api-mock/src/test/java/se/bjurr/wiremock/example/api/mock/CreateMocksTest.java
@@ -6,73 +6,69 @@ import static com.github.tomakehurst.wiremock.client.WireMockJaxrs.invocation;
 import static javax.servlet.http.HttpServletResponse.SC_ACCEPTED;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.logging.Logger;
-
-import org.junit.Test;
-
-import se.bjurr.wiremock.example.api.ExampleResource;
-import se.bjurr.wiremock.example.api.dto.ItemDTO;
-
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.Options;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+import org.junit.Test;
+import se.bjurr.wiremock.example.api.ExampleResource;
+import se.bjurr.wiremock.example.api.dto.ItemDTO;
 
 public class CreateMocksTest {
-	private static Logger LOG = Logger.getLogger(CreateMocksTest.class
-			.getName());
+  private static Logger LOG = Logger.getLogger(CreateMocksTest.class.getName());
 
-	public static String getMappingsRootPath() throws URISyntaxException {
-		final Path storagePath = Paths.get(Main.class.getResource(
-				"/root/mappings/.gitkeep").toURI());
-		final Path mappingsRoot = storagePath.getParent().getParent();
-		final String mappingsRootPath = mappingsRoot.toFile().getPath();
-		LOG.info("Storing Wiremock mappings in: " + mappingsRootPath);
-		return mappingsRootPath;
-	}
+  public static String getMappingsRootPath() throws URISyntaxException {
+    final Path storagePath = Paths.get(Main.class.getResource("/root/mappings/.gitkeep").toURI());
+    final Path mappingsRoot = storagePath.getParent().getParent();
+    final String mappingsRootPath = mappingsRoot.toFile().getPath();
+    LOG.info("Storing Wiremock mappings in: " + mappingsRootPath);
+    return mappingsRootPath;
+  }
 
-	@Test
-	public void createMocks() throws URISyntaxException {
-		final String mappingsRootPath = CreateMocksTest.getMappingsRootPath();
+  @Test
+  public void createMocks() throws URISyntaxException {
+    final String mappingsRootPath = CreateMocksTest.getMappingsRootPath();
 
-		final Options options = new WireMockConfiguration() //
-				.dynamicPort() //
-				.withRootDirectory(mappingsRootPath);
+    final Options options =
+        new WireMockConfiguration() //
+            .dynamicPort() //
+            .withRootDirectory(mappingsRootPath);
 
-		final WireMockServer wm = new WireMockServer(options);
-		wm.start();
-		WireMock.configureFor(wm.port());
+    final WireMockServer wm = new WireMockServer(options);
+    wm.start();
+    WireMock.configureFor(wm.port());
 
-		createStubs();
+    createStubs();
 
-		wm.saveMappings();
-	}
+    wm.saveMappings();
+  }
 
-	private void createStubs() {
+  private void createStubs() {
 
-		/**
-		 * Try it out with:<bt>
-		 *
-		 * <pre>
-		 * curl -v -H "Accept: application/json" http://localhost:8080/get?filter1=abc
-		 * curl -v -H "Accept: application/json" http://localhost:8080/get?filter1=some+attr+value
-		 * </pre>
-		 *
-		 */
-		stubFor(invocation(ExampleResource.class, (r) -> r.getItem("abc")) //
-				.willReturn(aResponse().withStatus(SC_NOT_FOUND)));
+    /**
+     * Try it out with:<bt>
+     *
+     * <pre>
+     * curl -v -H "Accept: application/json" http://localhost:8080/get?filter1=abc
+     * curl -v -H "Accept: application/json" http://localhost:8080/get?filter1=some+attr+value
+     * </pre>
+     */
+    stubFor(
+        invocation(ExampleResource.class, (r) -> r.getItem("abc")) //
+            .willReturn(aResponse().withStatus(SC_NOT_FOUND)));
 
-		for (final ItemDTO itemDto : MockFactory.getAllItems()) {
-			stubFor(invocation(ExampleResource.class,
-					(r) -> r.getItem(itemDto.getAttr1())) //
-					.willReturn(aResponse().withStatus(SC_ACCEPTED), itemDto));
+    for (final ItemDTO itemDto : MockFactory.getAllItems()) {
+      stubFor(
+          invocation(ExampleResource.class, (r) -> r.getItem(itemDto.getAttr1())) //
+              .willReturn(aResponse().withStatus(SC_ACCEPTED), itemDto));
 
-			stubFor(invocation(ExampleResource.class,
-					(r) -> r.createItem(itemDto)) //
-					.willReturn(aResponse().withStatus(SC_ACCEPTED)));
-		}
-	}
+      stubFor(
+          invocation(ExampleResource.class, (r) -> r.createItem(itemDto)) //
+              .willReturn(aResponse().withStatus(SC_ACCEPTED)));
+    }
+  }
 }
